3e7174db9f7afed2d183784716e2397071decfc3
Goran Rafael <rafaelgoran@gmail.com>
2022-03-18 14:00:28 -0300

    H2 jwt frame allow

--- src/main/java/com/demo/bci/jwt/config/WebSecurityConfig.java
+++ src/main/java/com/demo/bci/jwt/config/WebSecurityConfig.java
@@ -68,6 +68,7 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
 
 		// Add a filter to validate the tokens with every request
 		httpSecurity.addFilterBefore(jwtRequestFilter, UsernamePasswordAuthenticationFilter.class);
+		httpSecurity.headers().frameOptions().disable();
 	}
 
 
